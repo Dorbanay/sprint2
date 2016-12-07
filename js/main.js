f949002eb5984387c7168abe269bb1fd8ca98a2d
Dor Banay <banaydor@gmail.com>
2016-12-07 14:09:50 +0200

    exportImgToCanvas

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 console.log('Hello Meme');
 
 var gHex;
+var canvas;
+var ctx;
+
 // $(document).ready(function () {
 //     $('#characterLeft').text('140 characters left');
 //     $('#message').keydown(function () {
@@ -21,9 +24,17 @@ var gHex;
 //     });
 // });
 
+
+
+
+
+
+
 function init() {
     gHex = createHexagons();
     render();
+    canvas = document.querySelector('.canvas');
+    ctx = canvas.getContext('2d');
 }
 
 
@@ -53,7 +64,7 @@ function render() {
         var id = hexagon.id;
         var url = hexagon.url;
         var keywords = hexagon.keywords;
-        strHtml += '<div class="hexagon" style="background-image: url(' + url + ')" id="' + id + '">';
+        strHtml += '<div class="hexagon img-resposive" onclick="doCanvas(' + id + ')" style="background-image: url(' + url + ')" id="' + id + '">';
 
         strHtml += '' + '<div class="face1"></div><div class="face2"></div></div>';
 
@@ -70,7 +81,17 @@ function render() {
 
 
 /// ===== main ===== ////
+function doCanvas(imgId) {
+    
+    var img = new Image();
+    img.src = '../assets/imgs/img' + imgId + '.jpg';
 
+    img.onload = function () {
+        ctx.drawImage(img, 0, 0, 568, 360);
+        ctx.font = "60px 'Segoe UI'";
+        ctx.fillText("", 50, 300);
+    };
+}
 
 
 function getGallerySection() {
@@ -78,3 +99,42 @@ function getGallerySection() {
     // console.log(x);
     elMainGallery.addClass('displaynone');
 }
+
+
+
+
+
+
+// function init() {
+//     canvas = document.getElementById('canvas');
+//     ctx = canvas.getContext('2d');
+//     drawOnCanvas();
+// }
+
+
+
+/**
+* Demonstrates how to download a canvas an image with a single
+* direct click on a link.
+*/
+function drawOnCanvas(id) {
+    var img = new Image();
+    img.src = "'+id'";
+
+    img.onload = function () {
+        ctx.drawImage(img, 0, 0, 568, 360);
+        ctx.font = "60px 'Segoe UI'";
+        ctx.fillText("print on Canvas", 50, 300);
+    };
+}
+
+/**
+* This is the function that will take care of image extracting and
+* setting proper filename for the download.
+* IMPORTANT: Call it from within a onclick event.
+*/
+// function downloadImg(elLink) {
+//     elLink.href = canvas.toDataURL();
+//     elLink.download = 'perfectMeme.jpg';
+// }
+
