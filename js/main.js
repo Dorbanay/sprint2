05bee415be40c3d6370e8837b2fe554ad49b3cb0
Dor Banay <banaydor@gmail.com>
2016-12-07 11:38:58 +0200

    before rendering strag

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 console.log('Hello Meme');
 
 
-$(document).ready(function(){ 
+$(document).ready(function () {
     $('#characterLeft').text('140 characters left');
     $('#message').keydown(function () {
         var max = 140;
@@ -10,13 +10,37 @@ $(document).ready(function(){
         if (len >= max) {
             $('#characterLeft').text('You have reached the limit');
             $('#characterLeft').addClass('red');
-            $('#btnSubmit').addClass('disabled');            
-        } 
+            $('#btnSubmit').addClass('disabled');
+        }
         else {
             var ch = max - len;
             $('#characterLeft').text(ch + ' characters left');
             $('#btnSubmit').removeClass('disabled');
-            $('#characterLeft').removeClass('red');            
+            $('#characterLeft').removeClass('red');
         }
-    });    
+    });
 });
+
+
+
+/// ===== main ===== ////
+
+
+function exportImgToMeme(hexagon) {
+    getGallerySection();
+    var elImg = $('.img1');
+
+
+
+}
+
+
+function getGallerySection() {
+    var elMainGallery = $('.main-gallery');
+    // console.log(x);
+    elMainGallery.addClass('displaynone');
+}
+
+function getHexgonClass() {
+
+}
